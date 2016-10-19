4b9c554a28df216a24065bdd40b6f0bcaf0977f7
kishorekumar522 <kishorekumar522@gmail.com>
2016-10-19 22:43:21 +0530

    Adding SLF4J logging features Test

--- scalademo/src/main/scala/com/ykk/Demo/SLf4JTest.scala
+++ scalademo/src/main/scala/com/ykk/Demo/SLf4JTest.scala
@@ -1,6 +1,5 @@
 package com.ykk.Demo
 
-import org.apache.log4j.PropertyConfigurator
 import org.slf4j.LoggerFactory
 
 /**
@@ -10,7 +9,10 @@ object SLf4JTest{
 
   def main(args: Array[String]) {
 
-  PropertyConfigurator.configure(System.getProperty("log4j.configuration"))
+//  can use propertyConfigurator or vm arguments as below
+//  -Dlog4j.configuration=file:///Users/kishoreyakkala/git/ScalaLearning/scalademo/src/main/conf/log4j.properties
+//  PropertyConfigurator.configure(System.getProperty("log4j.configuration"))
+
   val logger = LoggerFactory.getLogger(this.getClass)
   logger.info(s"logger property : ${System.getProperty("log4j.configuration")}")
   logger.info("Hello SLF4J Test")
